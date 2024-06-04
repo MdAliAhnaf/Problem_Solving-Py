3e75270e332829cc0569c02f6e094d77425782e8
Md. Ali Ahnaf <aliahnaf2012@gmail.com>
2024-06-05 02:13:03 +0600

    v1

---
+++ contains_duplicate.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        hashset = set()
+        for x in nums:
+            if x in hashset:
+                return True
+            hashset.add(x)
+        return False
+        
+
+    
+
+
+
+
+    
+
+
