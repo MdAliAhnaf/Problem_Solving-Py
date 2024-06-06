9a47fdbe0e20b9dbcd47fada170df6822a48931e
Md. Ali Ahnaf <aliahnaf2012@gmail.com>
2024-06-06 10:08:40 +0600

    1

---
+++ factorial.py
@@ -0,0 +1,42 @@
+# i = 1
+# result = 1
+
+# for i in range(4):
+    
+#     i+=1
+#     result*=i
+    
+#     print(i)
+
+# print('The factorial of 4 is: {}'.format(result))
+
+
+i = 1
+result = 1
+
+for i in range(1,5,1):
+    
+    result*=i
+
+print('The factorial of 4 is: {}'.format(result))
+
+
+print(end = '\n')
+
+# result = 1
+# i=1
+
+# while i<=4:
+
+#     #result = result * i 
+#     #i = i + 1
+
+#     result *= i
+    
+#     # if i == 4:
+#     #     print('Loop upto fac 4 reached! Stopping execution..')
+#     #     break
+    
+#     i += 1
+
+# print('The factorial of 4 is: {}'.format(result))
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,13 @@
+i = 1
+result = 1
+
+for i in range(4):
+    
+    i+=1
+    result*=i
+    
+    print(i)
+
+print('The factorial of 4 is: {}'.format(result))
+
+print(end = '\n')
